4f3522629bf4949377f4ddbd9bccabf4cd88149d
mikaltedros <mtedros@terpmail.umd.edu>
2023-02-16 12:03:58 -0500

    added an about html file

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World from Mikal Tedros! I am adding my first code change'
-
+@app.route('/about')
+def about():
+    return render_template('about.html')
 @app.route('/hello')
 def hello():
     return render_template('hello.html')
